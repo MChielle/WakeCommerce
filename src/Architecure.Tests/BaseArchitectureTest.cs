8231474d5dede44fb7ba03e5ad66310afd63ad43
Matheus Pires Chielle <matheuschielle@gmail.com>
2025-09-29 18:59:53 -0300

    Add architecture tests.

---
+++ src/Architecure.Tests/BaseArchitectureTest.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.Application.Abstractions.Requests;
+using Catalog.Domain.Entities;
+using Catalog.Infrastructure.Database;
+using System.Reflection;
+
+namespace Architecture.Tests
+{
+    public abstract class BaseArchitectureTest
+    {
+        protected static readonly Assembly DomainAssembly = typeof(Entity).Assembly;
+        protected static readonly Assembly ApplicationAssembly = typeof(ICommand).Assembly;
+        protected static readonly Assembly InfrastructureAssembly = typeof(ApplicationDbContext).Assembly;
+        protected static readonly Assembly PresentationAssembly = typeof(Program).Assembly;
+    }
+}
---
+++ src/Architecure.Tests/Layers/LayerDepencencyTests.cs
@@ -0,0 +1,73 @@
+﻿namespace Architecture.Tests.Layers
+{
+    public class LayerDepencencyTests : BaseArchitectureTest
+    {
+        [Fact]
+        public void Domain_Should_Not_Depend_On_Any_Other_Layer()
+        {
+            var result = DomainAssembly
+                .GetReferencedAssemblies()
+                .Where(x =>
+                    x.Name == ApplicationAssembly.GetName().Name
+                    ||
+                    x.Name == InfrastructureAssembly.GetName().Name
+                    ||
+                    x.Name == PresentationAssembly.GetName().Name
+                );
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void Application_Should_Not_Depend_On_Infrastructure_Or_Presentation()
+        {
+            var result = ApplicationAssembly
+                .GetReferencedAssemblies()
+                .Where(x =>
+                    x.Name == DomainAssembly.GetName().Name
+                    ||
+                    x.Name == InfrastructureAssembly.GetName().Name
+                    ||
+                    x.Name == PresentationAssembly.GetName().Name
+                );
+            Assert.True(result.Count() == 1);
+            Assert.Matches(result.FirstOrDefault()?.Name ?? "", DomainAssembly.GetName().Name);
+        }
+
+        [Fact]
+        public void Infrastructure_Should_Not_Depend_On_Presentation_Or_Domain()
+        {
+            var result = InfrastructureAssembly
+                .GetReferencedAssemblies()
+                .Where(x =>
+                    x.Name == DomainAssembly.GetName().Name
+                    ||
+                    x.Name == ApplicationAssembly.GetName().Name
+                    ||
+                    x.Name == PresentationAssembly.GetName().Name
+                );
+
+            Assert.True(result.Count() == 2);
+            Assert.True(result?.Any(x => x.Name == DomainAssembly.GetName().Name) ?? false);
+            Assert.True(result?.Any(x => x.Name == ApplicationAssembly.GetName().Name) ?? false);
+        }
+
+        [Fact]
+        public void Presentation_Should_Not_Depend_On_Application_Or_Domain()
+        {
+            var result = PresentationAssembly
+                .GetReferencedAssemblies()
+                .Where(x =>
+                    x.Name == DomainAssembly.GetName().Name
+                    ||
+                    x.Name == ApplicationAssembly.GetName().Name
+                    ||
+                    x.Name == InfrastructureAssembly.GetName().Name
+                );
+
+            Assert.True(result.Count() == 2);
+            Assert.True(result?.Any(x => x.Name == InfrastructureAssembly.GetName().Name) ?? false);
+            Assert.True(result?.Any(x => x.Name == ApplicationAssembly.GetName().Name) ?? false);
+        }
+    }
+}
